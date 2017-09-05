d371e9a66dbbfa542bf1108569060d4ad496232a
박찬신 <parkcs@neutti.com>
2017-09-05 19:23:14 +0900

    ..

--- src/main/java/com/neutti/webframe/gradle/RunTask.java
+++ src/main/java/com/neutti/webframe/gradle/RunTask.java
@@ -1,8 +1,10 @@
 package com.neutti.webframe.gradle;
 
 import org.gradle.api.file.FileCollection;
+import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.tasks.Classpath;
 import org.gradle.api.tasks.JavaExec;
+import org.gradle.api.tasks.SourceSet;
 
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -15,16 +17,8 @@ public class RunTask extends JavaExec {
     public void exec() {
         setGroup("application");
         setMain("com.neutti.webframe.start.NeuttiWebApplication");
-        ClassLoader cl = ClassLoader.getSystemClassLoader();
-        URL[] urls = ((URLClassLoader)cl).getURLs();
-        List files = new ArrayList();
-        for(URL url: urls){
-            files.add(url.getFile());
-        }
-        setClasspath(getProject().fileTree(files));
-     /*   Classpath = sourceSets.main.runtimeClasspath
-        main = "com.neutti.webframe.start.NeuttiWebApplication"
-        group = 'application'*/
+        JavaPluginConvention javaPluginConvention = getProject().getConvention().getPlugin(JavaPluginConvention.class);
+        setClasspath(javaPluginConvention.getSourceSets().getByName(SourceSet.MAIN_SOURCE_SET_NAME).getRuntimeClasspath());
         super.exec();
     }
 
