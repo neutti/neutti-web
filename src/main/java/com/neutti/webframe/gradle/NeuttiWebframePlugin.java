eb2862b9432fdd01d5662188b52540d29d3ace3b
박찬신 <parkcs@neutti.com>
2017-09-05 18:36:44 +0900

    ..

--- src/main/java/com/neutti/webframe/gradle/NeuttiWebframePlugin.java
+++ src/main/java/com/neutti/webframe/gradle/NeuttiWebframePlugin.java
@@ -1,11 +1,16 @@
 package com.neutti.webframe.gradle;
 
+import groovy.lang.Closure;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.tasks.TaskContainer;
 
 public class NeuttiWebframePlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
-        project.getTasks().create("neuttiWebAppRun");
+        TaskContainer container = project.getTasks();
+        Task task = container.getByName("neuttiWebAppRun");
+        container.create("neuttiWebAppRun5", (Closure) task);
     }
 }
