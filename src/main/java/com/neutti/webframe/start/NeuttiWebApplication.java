45a679ea4f80b69a0d47374e1a34e834d9f1c673
박찬신 <parkcs@neutti.com>
2018-02-06 01:38:12 +0900

    @EnableAutoConfiguration(exclude = {MybatisAutoConfiguration.class}) 추가

--- src/main/java/com/neutti/webframe/start/NeuttiWebApplication.java
+++ src/main/java/com/neutti/webframe/start/NeuttiWebApplication.java
@@ -1,11 +1,14 @@
 package com.neutti.webframe.start;
 
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = {"com.neutti.webframe.config","com.neutti.webframe.servlet"})
+@EnableAutoConfiguration(exclude = {MybatisAutoConfiguration.class})
 public class NeuttiWebApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
